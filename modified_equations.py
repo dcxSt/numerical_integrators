6c6aba8baf3d916827e88374e34d9c5d736ac381
dcxSt <stephen.fay@mail.mcgill.ca>
2020-06-17 00:06:43 -0400

    modified equation computation started

---
+++ modified_equations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 13:54:39 2020
+
+@author: steve
+"""
+
+# ALGORITHMS FOR FINDING MODIFIED EQUATIONS
+
+# %% KEPLER
+
+
+
+
+# %% N BODY
+
+
+
+
